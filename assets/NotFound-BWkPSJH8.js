8b8147dc914f89df4d028aa7d3698320e5274187
Pranav Dalvi <pranav.dalvi@zohomail.in>
2025-11-22 19:40:56 +0000

    using hashRouter

--- assets/NotFound-BWkPSJH8.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Cc-22H13.js";const r=()=>o.jsx("div",{children:"404 Not Found"});export{r as default};
---
+++ assets/NotFound-D_vryjBh.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B0apygzS.js";const r=()=>o.jsx("div",{children:"404 Not Found"});export{r as default};
